70cd69c5ea74ca451996a25a993d20e6ef235922
Lacey Sanderson <laceysanderson@781094.no-reply.drupal.org>
2014-11-04 15:07:09 -0600

    Cleaned up sequence and genotypes template including adding API functions to make the more maintainable.

--- theme/templates/nd_genotypes_feature_genotypes.tpl.php
+++ theme/templates/nd_genotypes_feature_genotypes.tpl.php
@@ -22,190 +22,94 @@
  * You should instead see the tripal_feature.nd_genotypes.tpl.php template
  *
  */
-$feature = $variables['node']->feature;
 
-// specify the number of genotypes to show by default and the unique pager ID
-$num_results_per_page = 25;
-$feature_pager_id = 15;
+if ($num_alleles) {
 
-$is_variant = FALSE;
-$is_marker = FALSE;
+  // the $headers array is an array of fields to use as the colum headers.
+  // additional documentation can be found here
+  // https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7
+  $headers = array('Marker Call (Allele)', 'Number of Germplasm');
 
-/////////////////////////////
-// CASE #1: Markers
+  // the $rows array contains an array of rows where each row is an array
+  // of values for each column of the table in that row.  Additional documentation
+  // can be found here:
+  // https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7
+  $rows = array();
 
-$marker_types = unserialize(variable_get('nd_genotypes_marker_types', 'a:0:{}'));
-if (in_array($feature->type_id->name, $marker_types)) {
+  foreach ($markers as $marker_id => $marker) {
+    $marker_feature_id = $marker->feature_id;
 
-  $is_marker = TRUE;
+    // Name of the marker
+    $marker_name[$marker_feature_id] = $marker->name;
+    if (isset($marker->nid)) {
+      $marker_name[$marker_feature_id] = l($marker_name[$marker_feature_id], 'node/'.$marker->nid);
+    }
 
-  $options = array(
-    'return_array' => 1,
-  );
-  $feature = tripal_core_expand_chado_vars($feature, 'table', 'feature_genotype', $options);
-  $feature_genotypes = $feature->feature_genotype->feature_id;
+    // Add in my pie chart :)
+    $view_output[$marker_feature_id] = '';
+    $view = views_get_view('nd_genotypes_feature_genotype_chart_pie');
+    if (!empty($view)) {
+      $view->display['default']->display_options['filters']['feature_id']['value']['value'] = $marker->feature_id;
+      $view_output[$marker_feature_id] = $view->preview('marker_block');
+    }
 
-  // get the total number of records
-  $total_records = (sizeof($feature_genotypes) < $num_results_per_page) ? sizeof($feature_genotypes) : $num_results_per_page;
-
-  // now iterate through the feature genotypes and print a paged table.
-  if (count($feature_genotypes) > 0) {
-    // the $headers array is an array of fields to use as the colum headers.
-    // additional documentation can be found here
-    // https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7
-    $headers = array('Variant Type', 'Marker Call (Allele)');
-
-    // the $rows array contains an array of rows where each row is an array
-    // of values for each column of the table in that row.  Additional documentation
-    // can be found here:
-    // https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7
+    // Add in allele rows for the table.
     $rows = array();
-
-    foreach($feature_genotypes as $feature_genotype) {
-      $genotype = $feature_genotype->genotype_id;
-
-
-      // get the genotype type
-      $type = 'N/A';
-      if ($genotype->type_id) {
-        $type = ucwords(preg_replace('/_/', ' ', $genotype->type_id->name));
-      }
-
-      // add the fields to the table row
+    foreach($alleles[$marker_feature_id] as $allele) {
       $rows[] = array(
-        $type,
-        $genotype->description,
+        $allele->allele,
+        $allele->num_germplasm
       );
     }
 
-  // Add in my pie chart :)
-  $view = views_get_view('nd_genotypes_feature_genotype_chart_pie');
-  if (!empty($view)) {
-    $view->display['default']->display_options['filters']['feature_id']['value']['value'] = $feature->feature_id;
-    print $view->preview('marker_block');
-  }
-  ?>
+    // the $table array contains the headers and rows array as well as other
+    // options for controlling the display of the table.  Additional
+    // documentation can be found here:
+    // https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7
+    $table = array(
+      'header' => $headers,
+      'rows' => $rows,
+      'attributes' => array(
+        'id' => 'tripal_genetic-table-genotypes',
+      ),
+      'sticky' => FALSE,
+      'caption' => '',
+      'colgroups' => array(),
+      'empty' => '',
+    );
 
-  <div class="tripal_feature-data-block-desc tripal-data-block-desc">The following <?php print number_format($total_records) ?> genotype(s) have been recorded for this feature.</div><?php
-
-  // the $table array contains the headers and rows array as well as other
-  // options for controlling the display of the table.  Additional
-  // documentation can be found here:
-  // https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7
-  $table = array(
-    'header' => $headers,
-    'rows' => $rows,
-    'attributes' => array(
-      'id' => 'tripal_genetic-table-genotypes',
-    ),
-    'sticky' => FALSE,
-    'caption' => '',
-    'colgroups' => array(),
-    'empty' => '',
-  );
-
-  // once we have our table array structure defined, we call Drupal's theme_table()
-  // function to generate the table.
-  print theme_table($table);
-
+    // once we have our table array structure defined, we call Drupal's theme_table()
+    // function to generate the table.
+    $table_output[$marker_feature_id] = theme_table($table);
   }
 }
-
-
-/////////////////////////////
-// CASE #2: Variants
-
-$variant_types = unserialize(variable_get('nd_genotypes_variant_types', 'a:0:{}'));
-if (in_array($feature->type_id->name, $variant_types)) {
-
-  $is_variant = TRUE;
-  $variant_charts = array();
-
-  $options = array( 'return_array' => 1 );
-  $feature = tripal_core_expand_chado_vars($feature, 'table', 'feature_relationship', $options);
-
-  if (isset($feature->all_relationships['object']['is marker of']['marker'])) {
-    $markers = $feature->all_relationships['object']['is marker of']['marker'];
 ?>
-  <div class="tripal_feature-data-block-desc tripal-data-block-desc">The following markers are associated with this variant and the genotypes for each marker are displayed below.</div>
+
+<?php if ($is_marker) : ?>
+
+  <?php print $view_output[$marker->feature_id]; ?>
+  <div class="tripal_feature-data-block-desc tripal-data-block-desc">
+    The following <?php print number_format($num_alleles[$marker->feature_id]) ?> genotype(s) have been recorded for this feature.
+  </div>
+  <?php print $table_output[$marker->feature_id]; ?>
+
+<?php elseif ($is_variant) : ?>
+
+  <div class="tripal_feature-data-block-desc tripal-data-block-desc">
+    The following markers are associated with this variant and the genotypes for each marker are displayed below.
+  </div>
   <table id="tripal_feature-variant_genotypes">
-<?php
-      // the $headers array is an array of fields to use as the colum headers.
-      // additional documentation can be found here
-      // https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7
-      $headers = array('Variant Type', 'Marker Call (allele)');
-
-      foreach ($markers as $marker) {
-
-        $marker_feature_id = $marker->record->subject_id->feature_id;
-        $marker_name = $marker->record->subject_id->name;
-        if (isset($marker->record->subject_id->nid)) {
-          $marker_name = l($marker_name, 'node/'.$marker->record->subject_id->nid);
-        }
-
-        $marker_genotypes = chado_generate_var('feature',array('feature_id' => $marker->record->subject_id->feature_id));
-        $marker_genotypes = chado_expand_var($marker_genotypes, 'table', 'feature_genotype', array('return_array' => TRUE));
-
-        $num_genotypes = sizeof($marker_genotypes->feature_genotype->feature_id);
-
-        $rows = array();
-
-        if (!empty($marker_genotypes->feature_genotype->feature_id)) {
-          foreach ($marker_genotypes->feature_genotype->feature_id as $k => $genotype) {
-            $rows[] = array(
-              $genotype->genotype_id->type_id->name,
-              $genotype->genotype_id->description
-            );
-          }
-        }
-?>
-    <tr>
-      <td class="layout genotypes-table">
-      <?php
-        // The table and marker description cell
-        print '<h3>'.$marker_name.'</h3>';
-
-        if (!empty($rows)) {
-          // the $table array contains the headers and rows array as well as other
-          // options for controlling the display of the table.  Additional
-          // documentation can be found here:
-          // https://api.drupal.org/api/drupal/includes%21theme.inc/function/theme_table/7
-          $table = array(
-            'header' => $headers,
-            'rows' => $rows,
-            'attributes' => array(
-              'id' => 'tripal_genetic-table-genotypes',
-            ),
-            'sticky' => FALSE,
-            'caption' => '',
-            'colgroups' => array(),
-            'empty' => '',
-          );
-
-          // once we have our table array structure defined, we call Drupal's theme_table()
-          // function to generate the table.
-          print theme_table($table);
-        }
-        else {
-          print '<p id="genotypes" class="no-alleles">No allele calls have been made available for this marker.</p>';
-        }
-      ?>
-      </td>
-      <td class="layout genotypes-pie">
-      <?php
-        // The Genotype Pie Chart cell
-        $view = views_get_view('nd_genotypes_feature_genotype_chart_pie');
-        if (!empty($view)) {
-          $view->display['default']->display_options['filters']['feature_id']['value']['value'] = $marker_feature_id;
-          print $view->preview('marker_block');
-        }
-      ?>
-      </td>
-    </tr>
-<?php
-      } //end of foreach marker
-?>
+    <?php foreach ($markers as $marker_id => $marker): ?>
+      <tr>
+        <td class="layout genotypes-table">
+          <h3><?php print $marker_name[$marker_id] ?></h3>
+          <?php print $table_output[$marker_id]; ?>
+        </td>
+        <td class="layout genotypes-pie">
+          <?php print $view_output[$marker_id]; ?>
+        </td>
+      </tr>
+    <?php endforeach; ?>
   </table>
-<?php
-  } // end of if markers associated with variant
-} //end of if variant
+
+<?php endif; ?>
--- theme/templates/nd_genotypes_feature_sequence.tpl.php
+++ theme/templates/nd_genotypes_feature_sequence.tpl.php
@@ -20,125 +20,30 @@
  *
  */
 
-$feature = $variables['node']->feature;
+if ($sequence) : ?>
+  <div class="tripal_feature-data-block-desc tripal-data-block-desc"></div>
 
-// we don't want to get the sequence for traditionally large types. They are
-// too big,  bog down the web browser, take longer to load and it's not
-// reasonable to print them on a page.
-$residues ='';
-if(strcmp($feature->type_id->name,'scaffold') !=0 and
-   strcmp($feature->type_id->name,'chromosome') !=0 and
-   strcmp($feature->type_id->name,'supercontig') !=0 and
-   strcmp($feature->type_id->name,'pseudomolecule') !=0) {
-  $feature = tripal_core_expand_chado_vars($feature,'field','feature.residues');
-  $residues = $feature->residues;
-}
-
-// get the sequence derived from alignments
-$feature = $variables['node']->feature;
-
-$type = 'other';
-$info = array();
-$info['sequence'] = NULL; // Needed for the check later
-
-// Types
-$variant_types = unserialize(variable_get('nd_genotypes_variant_types', 'a:0:{}'));
-$marker_types = unserialize(variable_get('nd_genotypes_marker_types', 'a:0:{}'));
-
-if (in_array($feature->type_id->name, $marker_types)) {
-  $type = 'marker';
-
-  $feature_loc = chado_generate_var(
-    'featureloc',
-    array('feature_id' => $feature->feature_id),
-    array('include_fk' => array('srcfeature_id' => array('type_id' => TRUE)))
-  );
-  $feature_loc = chado_expand_var($feature_loc,'field','feature.residues');
-  $parent_feature = $feature_loc->srcfeature_id;
-  $info['srcfeature_id'] = $parent_feature->feature_id;
-  $info['feature_position'] = $feature_loc->fmin;
-
-  $sequence_feature_name = $parent_feature->name;
-  $fasta_description = 'The following sequence is that of the parent ' . $parent_feature->type_id->name
-    . ' <strong>without any variants, including the current one, taken into account</strong>.';
-  $marked_description = 'The following sequence is that of the parent '
-    . $parent_feature->type_id->name . ' <strong>modified to highlight all of the known variants</strong>.
-    The main variant is displayed using the [allele1/allele2] notation and any other
-    variants are represented via their IUPAC code. The resulting FASTA is in the <strong>
-    format required by most marker development programs including that for KASP assays</strong>.';
-
-  $info['sequence'] = $parent_feature->residues;
-}
-elseif (in_array($feature->type_id->name, $variant_types)) {
-  $type = 'variant';
-
-  $feature_loc = chado_generate_var(
-    'featureloc',
-    array('feature_id' => $feature->feature_id),
-    array('include_fk' => array('srcfeature_id' => array('type_id' => TRUE)))
-  );
-  $feature_loc = chado_expand_var($feature_loc,'field','feature.residues');
-  $parent_feature = $feature_loc->srcfeature_id;
-  $info['srcfeature_id'] = $parent_feature->feature_id;
-  $info['feature_position'] = $feature_loc->fmin;
-
-  $sequence_feature_name = $parent_feature->name;
-  $fasta_description = 'The following sequence is that of the parent ' . $parent_feature->type_id->name
-    . ' <strong>without any variants, including the current one, taken into account</strong>.';
-  $marked_description = 'The following sequence is that of the parent '
-    . $parent_feature->type_id->name . ' modified to highlight all of the known variants.
-    The main variant is displayed using the [allele1/allele2] notation and any other
-    variants are represented via their IUPAC code. The resulting FASTA is in the format
-    required by most marker development programs including that for KASP assays.';
-
-  $info['sequence'] = $parent_feature->residues;
-}
-elseif (!empty($residues)) {
-  $type = 'sequence';
-
-  $sequence_feature_name = $feature->name;
-  $fasta_description = 'The following sequence is that of the ' . $feature->type_id->name
-    . ' <strong>without any variants taken into account</strong>.';
-  $marked_description = '';
-
-  $info['sequence'] = $feature->residues;
-  $info['feature_position'] = 0;
-}
-if ($info['sequence']) { ?>
-  <div class="tripal_feature-data-block-desc tripal-data-block-desc"></div> <?php
-
-  // Marked-up record
-  $sequence_details = nd_genotypes_markup_variant_sequence($feature->feature_id, $type, $info);
-  $markedup_sequence = $sequence_details['marked_up_sequence'];
-
-  $fasta_header = '>' . $sequence_feature_name . ':' . $sequence_details['start'] . '-' . $sequence_details['end'] . ' (' . $feature->uniquename . ': ' . $feature->type_id->name . ')';
-
-  $has_variants = TRUE;
-  if ($markedup_sequence == $sequence_details['shortened_sequence']) {
-    $has_variants = FALSE;
-  }
-  if ($markedup_sequence AND $has_variants) { ?>
+    <!-- Variant Marked-up Sequence -->
     <h3>Variant Marked-up Sequence</h3>
     <?php print '<p>' . $marked_description . '</p>'; ?>
 
     <div id="tripal_feature-fasta-record">
-    <div id="tripal_feature-sequence-header"><?php print $fasta_header; ?></div>
-    <pre id="tripal_feature-sequence-residues" class="variant-marked-up-sequence"><?php
-      print $markedup_sequence; ?>
-    </pre>
-    </div> <?php
-  }
-
-  // FASTA Record
-  ?>
+      <div id="tripal_feature-sequence-header"><?php print $fasta_header; ?></div>
+      <pre id="tripal_feature-sequence-residues" class="variant-marked-up-sequence">
+        <?php print $sequence_with_variants; ?>
+      </pre>
+    </div>
     <br />
+
+    <!-- Plain Sequence -->
     <h3>FASTA Record</h3>
     <?php print '<p>' . $fasta_description . '</p>'; ?>
 
     <div id="tripal_feature-fasta-record">
-    <div id="tripal_feature-sequence-header"><?php print $fasta_header; ?></div>
-    <pre id="tripal_feature-sequence-residues"><?php
-      print $sequence_details['shortened_sequence']; ?>
-    </pre>
-    </div><?php
-}
+      <div id="tripal_feature-sequence-header"><?php print $fasta_header; ?></div>
+      <pre id="tripal_feature-sequence-residues">
+        <?php print $sequence; ?>
+      </pre>
+    </div>
+
+<?php endif; ?>
