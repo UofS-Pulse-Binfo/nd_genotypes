e21e8d6c79044977449b079a761f18288f37826a
Lacey Sanderson <lacey.sanderson@usask.ca>
2019-04-08 14:43:41 -0600

    Add tests for checking the version.

--- tests/api/dbTest.php
+++ tests/api/dbTest.php
@@ -18,11 +18,30 @@ class dbTest extends TripalTestCase {
   use DBTransaction;
 
   /**
-   * Basic test example.
-   * Tests must begin with the word "test".
-   * See https://phpunit.readthedocs.io/en/latest/ for more information.
+   * Tests nd_genotypes_get_postgresql_version() 
+   * and nd_genotypes_recheck_postgresql_version().
    */
-  public function testBasicExample() {
-    $this->assertTrue(true);
+  public function testGetPsqlVersion() {
+
+    // Retrieve the version.
+    // Ensure we are not simply testing a cached version.
+    nd_genotypes_recheck_postgresql_version(); 
+    $result = nd_genotypes_get_postgresql_version();
+
+    // Check that a version was returned.
+    $this->assertNotEmpty($result, "Unable to detect PostgreSQL version.");
+    $this->assertIsFloat($result, "Returned PosgreSQL version was not a float.");
+
+    // Check that it is the correct version.
+    $full_version = db_query('SELECT version()')->fetchField();
+    $this->assertContains( (string)$result, $full_version,
+      "The reported version is not in the original version string from PosgreSQL.");
+
+    // Check that rechecking the version does not change it.
+    $first = $result;
+    nd_genotypes_recheck_postgresql_version();
+    $second = nd_genotypes_get_postgresql_version();
+    $this->assertEquals($first, $second, 
+      "Re-checking the postgreSQL version should not change the result.");
   }
 }
