ab68f142d4cf39e7e1af6de47e0eba0d0aab18e7
Lacey Sanderson <lacey.sanderson@usask.ca>
2019-10-19 09:29:45 -0600

    Add Database seeder for marker complex and test retrieval of markers.

---
+++ tests/DatabaseSeeders/GeneticMarkerSeeder.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace Tests\DatabaseSeeders;
+
+use StatonLab\TripalTestSuite\Database\Seeder;
+use Faker\Factory;
+
+class GeneticMarkerSeeder extends Seeder
+{
+    public $variant_id;
+    public $chromosome_id;
+    public $organism_id;
+    public $position;
+
+    /**
+     * Seeds the database with users.
+     *
+     * @return void
+     */
+    public function up() {
+      $faker = Factory::create();
+
+      // SQL for retrieving the cvterm (assumes sequence cv).
+      $get_type_sql = "SELECT cvterm_id FROM {cvterm} WHERE name=:type and cv_id IN (SELECT cv_id FROM {cv} WHERE name='sequence')";
+
+      // Generate a position for this marker.
+      $position = rand(1000,100000);
+      $this->position;
+
+      // Organism.
+      $organism = factory('chado.organism')->create();
+      $organism_id = $organism->organism_id;
+      $this->organism_id = $organism_id;
+
+      // Chromosome.
+      $chr_type_id = chado_query($get_type_sql,
+        [':type' => 'chromosome'])->fetchField();
+      $chr = factory('chado.feature')->create([
+        'organism_id' => $organism->organism_id,
+        'type_id' => $chr_type_id,
+      ]);
+      $chr_id = $chr->feature_id;
+      $this->chromosome_id = $chr_id;
+
+      // Variant.
+      $variant_type_id = chado_query($get_type_sql,
+        [':type' => 'sequence_variant'])->fetchField();
+      $variant = factory('chado.feature')->create([
+        'organism_id' => $organism->organism_id,
+        'type_id' => $variant_type_id,
+      ]);
+      $variant_id = $variant->feature_id;
+      $this->variant_id = $variant_id;
+
+      // -- with position on chromosome.
+      $pos_variant = chado_insert_record('featureloc', [
+        'srcfeature_id' => $chr_id,
+        'feature_id' => $variant_id,
+        'fmin' => $position,
+        'fmax' => $position+1,
+      ]);
+
+      // -- and a variant type (e.g. SNP).
+      $variant_type_id = nd_genotypes_get_type_id('Variant Type');
+      $variant_prop = chado_insert_record('featureprop', [
+        'feature_id' => $variant_id,
+        'type_id' => $variant_type_id,
+        'value' => 'SNP',
+      ]);
+
+      // Genetic Marker.
+      $marker_type_id = chado_query($get_type_sql,
+        [':type' => 'genetic_marker'])->fetchField();
+      $marker = factory('chado.feature')->create([
+        'organism_id' => $organism->organism_id,
+        'type_id' => $marker_type_id,
+      ]);
+      $marker_id = $marker->feature_id;
+
+      // -- with position on chromosome.
+      $pos_marker = chado_insert_record('featureloc', [
+        'srcfeature_id' => $chr_id,
+        'feature_id' => $marker_id,
+        'fmin' => $position,
+        'fmax' => $position+1,
+      ]);
+
+      // -- and a marker type (e.g. Exome Capture).
+      $marker_type_id = nd_genotypes_get_type_id('Marker Type');
+      $marker_prop = chado_insert_record('featureprop', [
+        'feature_id' => $marker_id,
+        'type_id' => $marker_type_id,
+        'value' => $faker->words(3, TRUE),
+      ]);
+
+      // -- relationship with variant.
+      $rel_type = variable_get('nd_genotypes_rel_type_id', 0);
+      if (!$rel_type) {
+        $rel_type = chado_query($get_type_sql,
+          [':type' => 'variant_of'])->fetchField();
+        variable_set('nd_genotypes_rel_type_id', $rel_type);
+      }
+      $variant_position = variable_get('nd_genotypes_rel_position', 'subject');
+      variable_set('nd_genotypes_rel_position', $variant_position);
+      if ($variant_position == 'subject') {
+        $rel = chado_insert_record('feature_relationship', [
+          'subject_id' => $variant_id,
+          'type_id' => $rel_type,
+          'object_id' => $marker_id,
+        ]);
+      }
+      else {
+        $rel = chado_insert_record('feature_relationship', [
+          'subject_id' => $marker_id,
+          'type_id' => $rel_type,
+          'object_id' => $variant_id,
+        ]);
+      }
+
+      return [
+        'organism' => (array) $organism,
+        'chromosome' => [
+          'chromosome_id' => $chr_id,
+          'record' => $chr,
+        ],
+        'variant' => [
+          'variant_id' => $variant_id,
+          'record' => $variant
+        ],
+        'variant_type' => [
+          'record' => $variant_prop,
+          'type' => $variant_prop['value'],
+        ],
+        'marker' => [
+          'marker_id' => $marker_id,
+          'record' => $marker,
+        ],
+        'marker_type' => [
+          'record' => $marker_prop,
+          'type' => $marker_prop['value'],
+        ],
+        'relationship' => [
+          'type_id' => $rel_type,
+          'variant_position' => $variant_position,
+          'record' => $rel,
+        ],
+        'position' => [
+          'start' => $position,
+          'stop' => $position+1,
+          'records' => [
+            'marker' => $pos_marker,
+            'variant' => $pos_variant,
+          ],
+        ],
+      ];
+    }
+
+    /**
+     * Add a second marker to the same variant.
+     */
+    public function addMarker() {
+      $faker = Factory::create();
+
+      // SQL for retrieving the cvterm (assumes sequence cv).
+      $get_type_sql = "SELECT cvterm_id FROM {cvterm} WHERE name=:type and cv_id IN (SELECT cv_id FROM {cv} WHERE name='sequence')";
+
+      // Genetic Marker.
+      $marker_type_id = chado_query($get_type_sql,
+        [':type' => 'genetic_marker'])->fetchField();
+      $marker = factory('chado.feature')->create([
+        'organism_id' => $this->organism_id,
+        'type_id' => $marker_type_id,
+      ]);
+      $marker_id = $marker->feature_id;
+
+      // -- with position on chromosome.
+      $pos_marker = chado_insert_record('featureloc', [
+        'srcfeature_id' => $this->chromosome_id,
+        'feature_id' => $marker_id,
+        'fmin' => $this->position,
+        'fmax' => $this->position+1,
+      ]);
+
+      // -- and a marker type (e.g. Exome Capture).
+      $marker_type_id = nd_genotypes_get_type_id('Marker Type');
+      $marker_prop = chado_insert_record('featureprop', [
+        'feature_id' => $marker_id,
+        'type_id' => $marker_type_id,
+        'value' => $faker->words(3, TRUE),
+      ]);
+
+      // -- relationship with variant.
+      $rel_type = variable_get('nd_genotypes_rel_type_id', 0);
+      $variant_position = variable_get('nd_genotypes_rel_position', 'subject');
+      if ($variant_position == 'subject') {
+        $rel = chado_insert_record('feature_relationship', [
+          'subject_id' => $this->variant_id,
+          'type_id' => $rel_type,
+          'object_id' => $marker_id,
+        ]);
+      }
+      else {
+        $rel = chado_insert_record('feature_relationship', [
+          'subject_id' => $marker_id,
+          'type_id' => $rel_type,
+          'object_id' => $this->variant_id,
+        ]);
+      }
+
+      return [
+        'marker_id' => $marker_id,
+        'record' => $marker,
+        'type' => $marker_prop['value'],
+      ];
+
+    }
+}
--- tests/api/featuresTest.php
+++ tests/api/featuresTest.php
@@ -3,6 +3,7 @@ namespace Tests\api;
 
 use StatonLab\TripalTestSuite\DBTransaction;
 use StatonLab\TripalTestSuite\TripalTestCase;
+use  Tests\DatabaseSeeders\GeneticMarkerSeeder;
 
 /**
  * Tests the following API functions:
@@ -17,11 +18,113 @@ class featuresTest extends TripalTestCase {
   use DBTransaction;
 
   /**
-   * Basic test example.
-   * Tests must begin with the word "test".
-   * See https://phpunit.readthedocs.io/en/latest/ for more information.
+   * Tests nd_genotypes_get_marker().
+   *
+   * @group markers
    */
-  public function testBasicExample() {
-    $this->assertTrue(true);
+  public function testGetMarker() {
+
+    // There is configuration that allows the relationship connecting
+    // markers/variants to be either variant => marker or marker => variant.
+    // We need to test both configurations.
+    foreach(['subject', 'object'] as $variant_position) {
+
+      // Now create a fake term to remove assumptions.
+      $term = factory('chado.cvterm')->create();
+      $rel_type = $term->cvterm_id;
+      variable_set('nd_genotypes_rel_type_id', $rel_type);
+      variable_set('nd_genotypes_rel_position', $variant_position);
+
+      // Create a marker/variant dataset.
+      $seeder = new GeneticMarkerSeeder();
+      $data = $seeder->up();
+
+      // Retrieve the marker.
+      $marker = nd_genotypes_get_marker($data['variant']['variant_id']);
+
+      $this->assertNotFalse($marker, "Unable to return the marker.");
+
+      // There should only be a single marker...
+      $this->assertIsObject($marker, "There should only be a single marker.");
+      $this->assertEquals($data['marker']['marker_id'], $marker->feature_id,
+        "We retrieved something but it was not the marker we expected.");
+
+      // Now add a second marker.
+      $new_marker = $seeder->addMarker();
+      $markers = nd_genotypes_get_marker($data['variant']['variant_id'], FALSE, FALSE);
+
+      $this->assertNotFalse($markers, "Unable to return the marker set.");
+
+      // There should now be two markers...
+      $this->assertIsArray($markers, "There should be two markers.");
+      $this->assertArrayContainsObjectAttributeValue(
+        $markers, 'feature_id', $new_marker['marker_id'],
+        "We retrieved a set but it did not contain the marker we expected.");
+
+      // Now we should be able to ask for only one even though there are two.
+      // This is the default so do not pass additional params.
+      $marker = nd_genotypes_get_marker($data['variant']['variant_id']);
+      $this->assertNotFalse($marker, "Unable to return the marker.");
+      $this->assertIsObject($marker, "There should only be a single marker.");
+
+      // We can also ask for only the IDs so lets test that.
+      // Retrieve full marker set (2 markers).
+      $markers = nd_genotypes_get_marker($data['variant']['variant_id'], TRUE, FALSE);
+      $this->assertIsArray($markers, "There should be two markers.");
+      $this->assertEquals(
+        [$data['marker']['marker_id'], $new_marker['marker_id']],
+        $markers,
+        "The marker array does not contain the IDs we expect."
+      );
+
+      // Retrieve only a single marker ID.
+      $marker_id = nd_genotypes_get_marker($data['variant']['variant_id'], TRUE);
+      $this->assertIsNumeric($marker_id, "We did not recieve a valid marker ID.");
+    }
+  }
+
+  /**
+   * Tests nd_genotypes_get_type_id().
+   *
+   * @group markers
+   */
+  public function testGetTypeId() {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+  /**
+   * Tests nd_genotypes_get_marker_type().
+   *
+   * @group markers
+   */
+  public function testGetMarkerType() {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+  /**
+   * Tests nd_genotypes_get_variant().
+   *
+   * @group markers
+   */
+  public function testGetVariant() {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+  /**
+   * Tests nd_genotypes_get_variants_for_sequence().
+   *
+   * @group markers
+   */
+  public function testGetVariantsForSequence() {
+    $this->markTestIncomplete('This test has not been implemented yet.');
+  }
+
+  private function assertArrayContainsObjectAttributeValue($theArray, $attribute, $value) {
+    foreach($theArray as $arrayItem) {
+        if($arrayItem->$attribute == $value) {
+            return TRUE;
+        }
+    }
+    return FALSE;
   }
 }
