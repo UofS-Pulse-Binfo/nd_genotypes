a59b09f8fb077a755c4ab854da5a97442e945e97
Lacey Sanderson <lacey.sanderson@usask.ca>
2019-10-20 21:34:06 -0600

    Added genotype related tests and extended database seeder to support them.

--- tests/DatabaseSeeders/GeneticMarkerSeeder.php
+++ tests/DatabaseSeeders/GeneticMarkerSeeder.php
@@ -11,13 +11,16 @@ class GeneticMarkerSeeder extends Seeder
     public $chromosome_id;
     public $organism_id;
     public $position;
+    public $inital_data;
+    public $markers;
+    public $genotypes;
 
     /**
      * Seeds the database with users.
      *
      * @return void
      */
-    public function up() {
+    public function up($organism_id = NULL, $chr_id = NULL) {
       $faker = Factory::create();
 
       // SQL for retrieving the cvterm (assumes sequence cv).
@@ -28,19 +31,33 @@ class GeneticMarkerSeeder extends Seeder
       $this->position;
 
       // Organism.
-      $organism = factory('chado.organism')->create();
-      $organism_id = $organism->organism_id;
-      $this->organism_id = $organism_id;
+      if ($organism_id === NULL) {
+        $organism = factory('chado.organism')->create();
+        $organism_id = $organism->organism_id;
+        $this->organism_id = $organism_id;
+      }
+      else {
+        $this->organism_id = $organism_id;
+        $organism = chado_select_record('organism', ['*'], ['organism_id' => $organism_id]);
+        $organism = array_pop($organism);
+      }
 
       // Chromosome.
-      $chr_type_id = chado_query($get_type_sql,
-        [':type' => 'chromosome'])->fetchField();
-      $chr = factory('chado.feature')->create([
-        'organism_id' => $organism->organism_id,
-        'type_id' => $chr_type_id,
-      ]);
-      $chr_id = $chr->feature_id;
-      $this->chromosome_id = $chr_id;
+      if ($chr_id === NULL) {
+        $chr_type_id = chado_query($get_type_sql,
+          [':type' => 'chromosome'])->fetchField();
+        $chr = factory('chado.feature')->create([
+          'organism_id' => $organism->organism_id,
+          'type_id' => $chr_type_id,
+        ]);
+        $chr_id = $chr->feature_id;
+        $this->chromosome_id = $chr_id;
+      }
+      else {
+        $this->chromosome_id = $chr_id;
+        $chr = chado_select_record('feature', ['*'], ['feature_id' => $chr_id]);
+        $chr = array_pop($chr);
+      }
 
       // Variant.
       $variant_type_id = chado_query($get_type_sql,
@@ -153,6 +170,9 @@ class GeneticMarkerSeeder extends Seeder
           ],
         ],
       ];
+      $this->inital_data = $data;
+      $marker->marker_type = $marker_prop['value'];
+      $this->markers = [$marker];
 
       return $data;
     }
@@ -209,11 +229,163 @@ class GeneticMarkerSeeder extends Seeder
         ]);
       }
 
+      $marker->marker_type = $marker_prop['value'];
+      $this->markers[] = $marker;
       return [
         'marker_id' => $marker_id,
         'record' => $marker,
         'type' => $marker_prop['value'],
       ];
 
+    }
+
+    /**
+     * Add Genotypes to existing markers.
+     */
+    public function addGenotypes($max = 1) {
+
+      // Create a project.
+      $project = factory('chado.project')->create();
+
+      // Insert all the alleles.
+      $alleles = ['AA', 'TT', 'CC', 'GG', 'AT', 'AC', 'AG', 'TC', 'TG', 'CG'];
+      foreach ($alleles as $call) {
+        $genotype = chado_insert_record('genotype', [
+          'uniquename' => $project->project_id . '-' . $call,
+          'description' => $call,
+          'type_id' => ['name' => 'SNP', 'cv_id' => ['name' => 'sequence']],
+        ]);
+        $genotype_ids[$call] = $genotype['genotype_id'];
+      }
+
+      // For each set requested.
+      for ($i = 0; $i < $max; $i++) {
+
+        // Create Germplasm.
+        $germplasm = factory('chado.stock')->create([
+          'organism_id' => $this->organism_id,
+        ]);
+
+        // Create Sample.
+        $sample = factory('chado.stock')->create([
+          'organism_id' => $this->organism_id,
+        ]);
+
+        // Link Sample => Germplasm.
+        $rel_type = nd_genotypes_get_type_id('Stock Relationship');
+        if ($rel_type['position'] == 'subject') {
+          $rel = chado_insert_record('stock_relationship', [
+            'subject_id' => $sample->stock_id,
+            'type_id' => $rel_type['type_id'],
+            'object_id' => $germplasm->stock_id,
+          ]);
+        }
+        else {
+          $rel = chado_insert_record('stock_relationship', [
+            'subject_id' => $germplasm->stock_id,
+            'type_id' => $rel_type['type_id'],
+            'object_id' => $sample->stock_id,
+          ]);
+        }
+
+        // For each marker...
+        foreach ($this->markers as $marker) {
+
+          // Pick an allele and make sure it exists.
+          $call = array_rand($genotype_ids);
+          $genotype_id = $genotype_ids[$call];
+
+          $genotype_call = chado_insert_record('genotype_call', [
+            'variant_id' => $this->variant_id,
+            'marker_id' => $marker->feature_id,
+            'genotype_id' => $genotype_id,
+            'project_id' => $project->project_id,
+            'stock_id' => $sample->stock_id,
+          ]);
+
+          $data = [
+            'germplasm' => $germplasm,
+            'sample' => $sample,
+            'marker' => $marker,
+            'allele' => $call,
+            'genotype_id' => $genotype_id,
+            'genotype_call' => $genotype_call,
+            'project' => $project,
+          ];
+          $this->genotypes[] = $data;
+        }
+      }
+      return $this->genotypes;
+    }
+
+    /**
+     * Save the data in the materialized views.
+     */
+    public function syncMviews($partition) {
+      $return = [];
+
+      $partition = strtolower($partition);
+
+      // Create tables.
+      $calls_table = 'mview_ndg_'.$partition.'_calls';
+      nd_genotypes_create_mview_ndg_calls($calls_table);
+      $variant_table = 'mview_ndg_'.$partition.'_variants';
+      nd_genotypes_create_mview_ndg_variants($variant_table);
+      $germplasm_table = 'mview_ndg_germplasm_genotyped';
+      nd_genotypes_create_mview_ndg_germplasm_genotyped($germplasm_table);
+
+      // Pull out the initial data as well.
+      $data = $this->inital_data;
+
+      // Populate the calls table.
+      foreach ($this->genotypes as $r) {
+        $values = [
+          'variant_id' => $this->variant_id,
+          'marker_id' => $r['marker']->feature_id,
+          'marker_name' => $r['marker']->name,
+          'marker_type' => $r['marker']->marker_type,
+          'stock_id' => $r['sample']->stock_id,
+          'stock_name' => $r['sample']->name,
+          'germplasm_id' => $r['germplasm']->stock_id,
+          'germplasm_name' => $r['germplasm']->name,
+          'project_id' => $r['project']->project_id,
+          'genotype_id' => $r['genotype_id'],
+          'allele_call' => $r['allele'],
+        ];
+        $call = chado_insert_record($calls_table, $values);
+        $return['calls'][] = (array) $call;
+
+        /* Not sure why this inconsistently fails.
+        $germplasm = [
+          ':germplasm_id' => $r['germplasm']->stock_id,
+          ':germplasm_name' => $r['germplasm']->name,
+          ':organism_id' => $data['organism']['organism_id'],
+          ':organism_genus' => $data['organism']['genus'],
+          ':partition' => $partition,
+        ];
+        chado_query("
+          INSERT INTO {".$germplasm_table."}
+            (germplasm_id, germplasm_name,
+              organism_id, organism_genus, partition)
+            VALUES (:germplasm_id, :germplasm_name,
+              :organism_id, :organism_genus, :partition)",
+          $germplasm);
+          */
+      }
+
+      // Finally populate the variant table.
+      $values = [
+        'variant_id' => $this->variant_id,
+        'variant_name' => $data['variant']['record']->name,
+        'variant_type' => $data['variant_type']['type'],
+        'srcfeature_id' => $data['chromosome']['chromosome_id'],
+        'srcfeature_name' => $data['chromosome']['record']->name,
+        'fmin' => $data['position']['start'],
+        'fmax' => $data['position']['stop'],
+      ];
+      $variant = chado_insert_record($variant_table, $values);
+      $return['variant'] = (array) $variant;
+
+      return $return;
     }
 }
